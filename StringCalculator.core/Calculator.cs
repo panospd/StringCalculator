c326883c4287c96404fdd041992f334296ed6000
panospd <panos.anastasiadis@cloudcommercegroup.com>
2021-05-21 14:21:28 +0100

    IMplemented added numbers provided as string with comma delimeter

--- StringCalculator.core/Calculator.cs
+++ StringCalculator.core/Calculator.cs
@@ -1,7 +1,20 @@
-﻿namespace StringCalculator.core
+﻿using System;
+using System.Linq;
+
+namespace StringCalculator.core
 {
     public class Calculator
     {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+                return default;
 
+            return numbers
+                .Split(',', '\n')
+                .Select(int.Parse)
+                .ToList()
+                .Sum();
+        }
     }
 }
--- StringCalculator.tests/CalculatorTests.cs
+++ StringCalculator.tests/CalculatorTests.cs
@@ -1,13 +1,26 @@
 ﻿using NUnit.Framework;
+using StringCalculator.core;
 
 namespace StringCalculator.tests
 {
     public class CalculatorTests
     {
+        Calculator sut;
+
         [SetUp]
         public void Setup()
         {
+            sut = new Calculator();
+        }
 
+        [TestCase("", ExpectedResult = 0)]
+        [TestCase("1", ExpectedResult = 1)]
+        [TestCase("1,2", ExpectedResult = 3)]
+        [TestCase("1,2,3,6,4,2,9", ExpectedResult = 27)]
+        [TestCase("1\n2,3", ExpectedResult = 6)]
+        public int Add_WhenInputIsEmpty_ShouldReturn0(string input)
+        {
+            return sut.Add(input);
         }
     }
 }
