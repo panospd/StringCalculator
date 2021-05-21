3c711b4910e5264d7105be7a0912284635ba4228
panospd <panos.anastasiadis@cloudcommercegroup.com>
2021-05-21 18:10:37 +0100

    Refactoring and cleanup

--- StringCalculator.core/helpers/Delimiters/HasCustomDelimiterExtention.cs
+++ StringCalculator.core/helpers/Delimiters/HasCustomDelimiterExtention.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-namespace StringCalculator.core.helpers
+﻿namespace StringCalculator.core.helpers
 {
     public static class HasCustomDelimiterExtention
     {
